fd33c910c788bba5e3af1b2cbdac17b8d70d733a
boomchanotai <boom9387@hotmail.com>
2024-06-15 16:13:43 +0700

    feat: add print button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,3 +19,25 @@
 		{/each}
 	</Container>
 {/each}
+<div class="no-print">
+	<div class="p-8 space-x-4">
+		<button
+			type="button"
+			on:click={() => {
+				window.print();
+			}}
+			class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+		>
+			Print
+		</button>
+
+		<a href="/มนเบเกอรี่color.ai.ps">
+			<button
+				type="button"
+				class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+			>
+				Download Logo
+			</button>
+		</a>
+	</div>
+</div>
